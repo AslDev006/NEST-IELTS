5a085d836680e7eaf1f2b97c5669270216749079
unknown <asldev006@gmail.com>
2024-02-07 18:35:21 +0500

    Gunicorn

--- Nest_IELTS/settings.py
+++ Nest_IELTS/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure--r5&8w4g6p3oat!a2_7!95z@fcjzl$^7gank7rdwgz7jh74ew!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -113,6 +113,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')# Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn "Nest_IELTS.wsgi"
